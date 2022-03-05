90c43ecff031c4f6491ce004a2971bb017cb42b4
Clayton Horning <clayton.horning@colorado.edu>
2022-03-05 12:34:39 -0700

    Changes

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: {
-    MONGO_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqbz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-  },
-};
